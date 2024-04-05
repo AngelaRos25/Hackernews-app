99b4a2555fcea769eebfee641ea4ca181a74fe00
Giangy25 <angirosace99@gmail.com>
2024-04-05 18:32:58 +0200

    aggiungo errore

--- src/js/element.js
+++ src/js/element.js
@@ -1,78 +1,77 @@
-
-const BoxNews = document.getElementById("box-news");//dove appendere le news
+const BoxNews = document.getElementById("box-news"); //dove appendere le news
 
 //creo i box per le news
-function createNewsEl(tagName, classList){
-  const element = document.createElement(tagName);
-  element.classList.add(classList);
-  return element
+function createNewsEl(tagName, classList) {
+    const element = document.createElement(tagName);
+    element.classList.add(classList);
+    return element
 };
 
 //creare 1 div News
 export function theOneDiv(newsTitle, newsLink, newsTime) {
-  const divOneNews = createNewsEl("a", "one-news");
-  divOneNews.href = newsLink;
-  divOneNews.target = "blank";
-  BoxNews.append(divOneNews);
-  
-  //creo il titolo
-  const TitleBox = createNewsEl("div", "title-box");
-  divOneNews.append(TitleBox);
-  
-  const titleN = document.createElement ("a");
-  titleN.classList.add("titleN"); 
-  titleN.text = newsTitle;
-  TitleBox.append(titleN);
-  
-  //creo il time
-  const linkTimeBox = createNewsEl("div", "time-link-box");
-  divOneNews.append(linkTimeBox);
+    const divOneNews = createNewsEl("a", "one-news");
+    divOneNews.href = newsLink;
+    divOneNews.target = "blank";
+    BoxNews.append(divOneNews);
 
-  let time = editDate(newsTime);
+    //creo il titolo
+    const TitleBox = createNewsEl("div", "title-box");
+    divOneNews.append(TitleBox);
 
-  const timeEl = document.createElement("p");
-  timeEl.classList.add ("time-element");
-  timeEl.innerText = `${time.date}/${time.month} - ${time.hour}:${time.minutes}`;
-  linkTimeBox.append(timeEl);
-  
-  //creo il link
-  const link = document.createElement ("a");
+    const titleN = document.createElement("a");
+    titleN.classList.add("titleN");
+    titleN.text = newsTitle;
+    TitleBox.append(titleN);
 
-  if(newsLink) {
-    link.innerText = cutUrl(newsLink);
-    link.classList.add ("link");
-    link.target = "blank"; 
-    link.href = newsLink;
-  } else {
-    link.classList.add ("link");
-    link.text = "not a link";
-  }
-  linkTimeBox.append(link);
+    //creo il time
+    const linkTimeBox = createNewsEl("div", "time-link-box");
+    divOneNews.append(linkTimeBox);
+
+    let time = editDate(newsTime);
+
+    const timeEl = document.createElement("p");
+    timeEl.classList.add("time-element");
+    timeEl.innerText = `${time.date}/${time.month} - ${time.hour}:${time.minutes}`;
+    linkTimeBox.append(timeEl);
+
+    //creo il link
+    const link = document.createElement("a");
+
+    if (newsLink) {
+        link.innerText = cutUrl(newsLink);
+        link.classList.add("link");
+        link.target = "blank";
+        link.href = newsLink;
+    } else {
+        link.classList.add("link");
+        link.text = "not a link";
+    }
+    linkTimeBox.append(link);
 }
 
 //formatto la data
 function editDate(timeForm) {
-  let timeDate = new Date (timeForm * 1000);
-  let edit = (num) => (num > 9 ? num : `0${num}`);
+    let timeDate = new Date(timeForm * 1000);
+    let edit = (num) => (num > 9 ? num : `0${num}`);
 
-  timeDate.month = edit(timeDate.getMonth() + 1);
-  timeDate.date = edit(timeDate.getDate());
-  timeDate. hour = edit(timeDate.getHours());
-  timeDate.minutes = edit(timeDate.getMinutes());
+    timeDate.month = edit(timeDate.getMonth() + 1);
+    timeDate.date = edit(timeDate.getDate());
+    timeDate.hour = edit(timeDate.getHours());
+    timeDate.minutes = edit(timeDate.getMinutes());
 
-  return timeDate;
+    return timeDate;
 }
 
 //funzione accorcia url news
 
-function cutUrl(newsLink){
-  if(newsLink.includes("http://")){
-    return newsLink.length > 22 ? newsLink.slice(7, 22) + "..." : newsLink
-  } else if(newsLink.includes("https://")) {
-    return newsLink.length > 20 ? newsLink.slice(7, 20) + "..." : newsLink
-  }
+function cutUrl(newsLink) {
+    if (newsLink.includes("http://")) {
+        return newsLink.length > 22 ? newsLink.slice(7, 22) + "..." : newsLink
+    } else if (newsLink.includes("https://")) {
+        return newsLink.length > 20 ? newsLink.slice(7, 20) + "..." : newsLink
+    }
 
-  let slashLink = newsLink.indexOf("/");
-  newsLink = newsLink.slice(7, slashLink);
-  return newsLink;
-  }
+    let slashLink = newsLink.indexOf("/");
+    newsLink = newsLink.slice(7, slashLink);
+    return newsLink;
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,15 @@
 //general import
 
-import '../css/style.css';//import css style
+import '../css/style.css'; //import css style
 
 // aggiungo axios e lodash
 import axios from 'axios';
-var _ = require('lodash');
+const _ = require('lodash');
 
 //importo i div dal file element.js
-import {theOneDiv} from "./element"
+import {
+    theOneDiv
+} from "./element"
 
 const getBtn = document.getElementById('button');
 const API_URL = 'https://hacker-news.firebaseio.com/v0';
@@ -16,32 +18,33 @@ let count = 10;
 let baseNum = 1;
 
 //contattare le API che restituirà 500 id
-async function loadApi(){
-  try {
-    const response = await axios.get(`${API_URL}/newstories.json`)
-    takeId(response.data);
-  } catch (error) {
-    console.error(error);
-  } 
+async function loadApi() {
+    try {
+        const response = await axios.get(`${API_URL}/newstories.json`)
+        takeId(response.data);
+    } catch (error) {
+        console.error("Error fetching news", error);
+        alert("Error while loading the news. Try again.");
+    }
 }
 loadApi();
 
-async function takeId(){
-  for (let i = baseNum; i < baseNum + count; i++){
-    let newsA = await axios.get(`${API_URL}/item/${i}.json`)
-    
-    let titleAct = _.get(newsA, "data.title");
-    let linkAct = _.get(newsA, "data.url");
-    let timeAct = _.get(newsA, "data.time");
+async function takeId() {
+    for (let i = baseNum; i < baseNum + count; i++) {
+        let newsA = await axios.get(`${API_URL}/item/${i}.json`)
 
-    theOneDiv(titleAct, linkAct, timeAct);
-  }
+        let titleAct = _.get(newsA, "data.title");
+        let linkAct = _.get(newsA, "data.url");
+        let timeAct = _.get(newsA, "data.time");
+
+        theOneDiv(titleAct, linkAct, timeAct);
+    }
 }
 
 
 //aggiungere pulsante load more
 getBtn.addEventListener("click", () => {
-baseNum = count;
-count += 0;
-takeId();
+    baseNum = count;
+    count += 0;
+    takeId();
 })
